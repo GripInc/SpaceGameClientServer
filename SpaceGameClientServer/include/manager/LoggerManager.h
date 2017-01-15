f5dc33f3a82babf78818106eabca3eab9f499bd4
ced <grip.inc@free.fr>
2017-01-15 12:59:54 +0100

    Add milliseconds marker in logs

--- SpaceGameClientServer/include/manager/LoggerManager.h
+++ SpaceGameClientServer/include/manager/LoggerManager.h
@@ -11,6 +11,7 @@ public:
 	void logE(const std::string& _logClass, const std::string& _logFunction, const std::string& _log, bool _addNewLine = false) const;
 	void logW(const std::string& _logClass, const std::string& _logFunction, const std::string& _log, bool _addNewLine = false) const;
 	void logI(const std::string& _logClass, const std::string& _logFunction, const std::string& _log, bool _forceStdOut, bool _addNewLine = false) const;
+	std::string getMilliseconds() const;
 
 protected:
 	LoggerManager() {}
